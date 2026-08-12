3aa9608983555b100bd7464734e5790beb6e1c04
mozydev <mouhamedseydinasylla@gmail.com>
2026-08-12 09:51:20 +0100

    Atelier 03 : Controller REST CRUD + Spring Data REST (pagination, tri, recherche, projections)

--- backend/src/main/java/com/mozy/produits/ProduitsApplication.java
+++ backend/src/main/java/com/mozy/produits/ProduitsApplication.java
@@ -1,13 +1,25 @@
 package com.mozy.produits;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+
+import com.mozy.produits.entities.Produit;
 
 @SpringBootApplication
-public class ProduitsApplication {
+public class ProduitsApplication implements CommandLineRunner {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ProduitsApplication.class, args);
-	}
+    @Autowired
+    private RepositoryRestConfiguration repositoryRestConfiguration;
 
+    public static void main(String[] args) {
+        SpringApplication.run(ProduitsApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        repositoryRestConfiguration.exposeIdsFor(Produit.class);
+    }
 }
